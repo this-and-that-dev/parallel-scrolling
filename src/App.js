ecb0d6537cf44154239e63ad8b8df9d17cc837e3
공수한 <ksh92315@pionnet.co.kr>
2024-10-19 00:17:34 +0900

    parallax scrolling 을 위한 기본 html 작업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <>
+            <div>
+                <img src="/grc-1973-c-03174orig.jpg" width="300" alt="" />
+            </div>
+            <p>
+                awefawefawefjiofawejoawefjopawefjfawej;klasdklfa
+                awefiajpweofiajw pefjawopejawef
+                aweifpawoejfiopwjef
+                awefawijpefioajefiopawpeifawe
+                apwoifjawiopfjopaiejfiopwef
+            </p>
+            <div>
+                <img src="/KSC-20241003-PH-JBP01_0001~large.jpg" width="300" alt="" />
+            </div>
+            <p>
+               fjijweiofjweiopfjweiopfjoawipfjf aweifpoawjefiopajef
+                awefiapjweiaw jefiajw eioajwiopjawiopawiofjadf
+                iopjai iwojf ipawejfa
+            </p>
+            <p>
+                wefawe;fjawe
+                fawefawejf i  jiejpfoiwjeifawe
+                fawe jfpioajefioawefawef
+                aw[efawefawefawiejfp ioef
+            </p>
+            <div>
+                <img src="/KSC-20241014-SPX02_0007~large.jpg" width="300" alt="" />
+            </div>
+            <p>
+                ajwieofja wioef
+            </p>
+            <p>
+            sdfajiwpefjawfawef
+            </p>
+        </>
+    );
 }
 
 export default App;
